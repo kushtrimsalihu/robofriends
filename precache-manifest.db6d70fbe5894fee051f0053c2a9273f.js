d758d280674628565c94025dac5b1d5e47f044da
unknown <kushtrim@gmail.com>
2020-09-14 05:17:39 +0200

    Updates

---
+++ precache-manifest.db6d70fbe5894fee051f0053c2a9273f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d4f04b2ad1ffaa2b17da17af857d39c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bfa7ebd5029e2cc44568",
+    "url": "/robofriends/static/css/main.acb62ee2.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfa7ebd5029e2cc44568",
+    "url": "/robofriends/static/js/main.cc8236d2.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "49c399b9574c7a1ca511737f50cb7d42",
+    "url": "/robofriends/static/media/Future-Earth.49c399b9.woff"
+  }
+]);
