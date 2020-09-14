fd49542a4ea9911616d22ef0d34a3108a03ced73
unknown <kushtrim@gmail.com>
2020-09-14 05:07:49 +0200

    first commit

---
+++ build/precache-manifest.aabeacdc300ecb689877b88f5a09ea65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435991881317933af5e19a67b697b6a3",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4d43e300413c615b311d",
+    "url": "/robofriends/static/css/main.acb62ee2.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d43e300413c615b311d",
+    "url": "/robofriends/static/js/main.8998fe64.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "49c399b9574c7a1ca511737f50cb7d42",
+    "url": "/robofriends/static/media/Future-Earth.49c399b9.woff"
+  }
+]);
